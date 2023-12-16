6e66c43f80b5397c1d4a9b7c810c7c70c283ce3c
Mustafa Faruk ESEN <pirifaruk@gmail.com>
2023-12-16 18:05:58 +0300

    create project from mfe

---
+++ web/backend/src/test/java/com/okuma/dostu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okuma.dostu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
