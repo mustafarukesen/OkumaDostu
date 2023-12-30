666414a2e7a622b41622231001296ff7da681106
Mustafa Faruk ESEN <pirifaruk@gmail.com>
2023-12-30 18:29:07 +0300

    updated

--- web/backend/src/main/java/com/okuma/dostu/backend/business/concretes/GoogleBookManager.java
+++ web/backend/src/main/java/com/okuma/dostu/backend/business/concretes/GoogleBookManager.java
@@ -43,10 +43,12 @@ public class GoogleBookManager implements GoogleBookService {
             for (JsonNode item : itemsArray) {
                 Book book = createBookFromJsonNode(item.path("volumeInfo"));
 
+                if (book.getCategory() == null || book.getPublisher() == null || book.getAuthor() == null)
+                    continue;
+
                 CreateBookRequest createBookRequest = modelMapperService.forRequest().map(book, CreateBookRequest.class);
                 bookService.add(createBookRequest);
 
-                // Diğer işlemler
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -56,87 +58,75 @@ public class GoogleBookManager implements GoogleBookService {
     private Book createBookFromJsonNode(JsonNode volumeInfo) {
         Book book = new Book();
 
-        String title = volumeInfo.path("title").asText();
-        String publisherName = volumeInfo.path("publisher").asText();
+        book.setTitle(volumeInfo.path("title").asText());
+        book.setPageCount(volumeInfo.path("pageCount").asInt());
+        book.setIsbn13(getIsbn13(volumeInfo.path("industryIdentifiers")));
+        book.setAuthor(getAuthor(volumeInfo.path("authors")));
+        book.setCategory(getCategory(volumeInfo.path("categories")));
+        book.setPublisher(getPublisher(volumeInfo.path("publisher")));
+        book.setDescription(volumeInfo.path("description").asText());
+        book.setThumbnail(volumeInfo.path("imageLinks").path("thumbnail").asText());
+
         String publishedDate = volumeInfo.path("publishedDate").asText();
-        String description = volumeInfo.path("description").asText();
-        String thumbnail = volumeInfo.path("imageLinks").path("thumbnail").asText();
-        int pageCount = volumeInfo.path("pageCount").asInt();
-
-        // ISBN-13 kontrolü
-        JsonNode identifiersArray = volumeInfo.path("industryIdentifiers");
-        for (JsonNode identifier : identifiersArray) {
-            if (identifier.has("type") && identifier.get("type").asText().equals("ISBN_13")) {
-                String isbn13 = identifier.get("identifier").asText();
-                book.setIsbn13(isbn13);
-            }
-        }
-
-        //Yazar kontrolü
-         JsonNode authorsArray = volumeInfo.path("authors");
-        if (authorsArray.isArray() && !authorsArray.isEmpty()) {
-            String authorName = authorsArray.get(0).asText();
-            Author author = authorRepository.findByName(authorName);
-            if (author == null) {
-                // Yazar yoksa, yeni bir yazar oluştur ve veritabanına ekle
-                //System.out.println("----Yazar yok----");
-                author = new Author();
-                author.setName(authorName);
-                //System.out.println("AuthorName 2 \n" + authorName);
-                authorRepository.save(author);
-            } else {
-                book.setAuthor(author);
-            }
-        }
-
-        // Kategori kontrolü
-        JsonNode categoriesArray = volumeInfo.path("categories");
-        if (categoriesArray.isArray() && !categoriesArray.isEmpty()) {
-            String categoryName = categoriesArray.get(0).asText();
-            System.out.println("-------- CategoryName: " + categoryName + " --------");
-            Category category = categoryRepository.findByName(categoryName);
-            if (category == null) {
-                // Kategori yoksa, yeni bir kategori oluştur ve veritabanına ekle
-                System.out.println("-------- Kategori yok --------");
-                category = new Category();
-                category.setName(categoryName);
-                System.out.println("-------- CategoryName 2: " + categoryName + " ------\n");
-                categoryRepository.save(category);
-            } else {
-                System.out.println("-------- Kategori var --------");
-                System.out.println("-------- CategoryName 3: " + category.getName() + " --------\n");
-                book.setCategory(category);
-            }
-
-        }
-
-        // Yayıncı kontrolü
-        Publisher publisher = publisherRepository.findByName(publisherName);
-        //System.out.println("-------- " + publisher + " ------------");
-        if (publisher == null) {
-            // Yayıncı yoksa, yeni bir yayıncı oluştur ve veritabanına ekle
-            //System.out.println("----Yayınevi yok----");
-            publisher = new Publisher();
-            publisher.setName(publisherName);
-            //System.out.println("PublisherName 2 \n" + publisherName);
-            publisherRepository.save(publisher);
-        } else {
-            //System.out.println("-----Yayınevi var-------\n");
-            book.setPublisher(publisher);
-        }
-
-        // Diğer bilgiler
-        book.setDescription(description);
-        book.setTitle(title);
-        book.setThumbnail(thumbnail);
-
-        // LocalDate olarak publishedDate kontrolü
         if (!publishedDate.isEmpty()) {
             book.setPublishedDate(LocalDate.parse(publishedDate));
         }
 
-        book.setPageCount(pageCount);
-
         return book;
+    }
+
+    private String getIsbn13(JsonNode identifiersArray) {
+        for (JsonNode identifier : identifiersArray) {
+            if (identifier.has("type") && identifier.get("type").asText().equals("ISBN_13")) {
+                return identifier.get("identifier").asText();
+            }
+        }
+        return null;
+    }
+
+    private Author getAuthor(JsonNode authorsArray) {
+        if (authorsArray.isArray() && !authorsArray.isEmpty()) {
+            String authorName = authorsArray.get(0).asText();
+            if (authorName != null) {
+                Author author = authorRepository.findByName(authorName);
+                if (author == null) {
+                    author = new Author();
+                    author.setName(authorName);
+                    authorRepository.save(author);
+                }
+                return author;
+            }
+        }
+        return null;
+    }
+
+    private Category getCategory(JsonNode categoriesArray) {
+        if (categoriesArray.isArray() && !categoriesArray.isEmpty()) {
+            String categoryName = categoriesArray.get(0).asText();
+            if (categoryName != null) {
+                Category category = categoryRepository.findByName(categoryName);
+                if (category == null) {
+                    category = new Category();
+                    category.setName(categoryName);
+                    categoryRepository.save(category);
+                }
+                return category;
+            }
+        }
+        return null;
+    }
+
+    private Publisher getPublisher(JsonNode publisherNode) {
+        String publisherName = publisherNode.asText();
+        if (publisherName != null) {
+            Publisher publisher = publisherRepository.findByName(publisherName);
+            if (publisher == null) {
+                publisher = new Publisher();
+                publisher.setName(publisherName);
+                publisherRepository.save(publisher);
+            }
+            return publisher;
+        }
+        return null;
     }
 }
